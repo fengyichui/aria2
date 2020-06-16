47d516a8ba0c0ce5aa96667701e03c41d3e75915
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2020-06-17 00:08:55 +0300

    flush internal buffers before auto-saving control file
    Otherwise, some pieces may be marked as finished in the control file
    though they have not yet been written to the storage file.
    
    This should prevent data loss and corruption when resuming downloads
    after an app crash.
    
    Signed-off-by: Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>

--- src/RequestGroup.cc
+++ src/RequestGroup.cc
@@ -211,7 +211,7 @@ std::pair<error_code::Value, std::string> RequestGroup::downloadResult() const
 void RequestGroup::closeFile()
 {
   if (pieceStorage_) {
-    pieceStorage_->flushWrDiskCacheEntry();
+    pieceStorage_->flushWrDiskCacheEntry(true);
     pieceStorage_->getDiskAdaptor()->closeFile();
   }
 }
@@ -1291,6 +1291,9 @@ bool RequestGroup::doesUploadSpeedExceed()
 void RequestGroup::saveControlFile() const
 {
   if (saveControlFile_) {
+    if (pieceStorage_) {
+      pieceStorage_->flushWrDiskCacheEntry(false);
+    }
     progressInfoFile_->save();
   }
 }
